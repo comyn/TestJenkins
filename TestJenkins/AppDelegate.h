94d0d8227f3794ccaa2635a1c980b71fe9b60a5d
comyn <comyn@Comyn-2.local>
2017-03-07 09:42:39 +0800

    Initial Commit

---
+++ TestJenkins/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestJenkins
+//
+//  Created by comyn on 2017/3/7.
+//  Copyright © 2017年 comyn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
